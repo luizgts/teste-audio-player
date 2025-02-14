f4e931f8724fbcb4564d03c2d34e20eafc1b6297
luiz.souza <lgsouza.sp@gmail.com>
2025-02-14 01:07:28 -0300

    Adiciona função de play, pause e mudança de track

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,10 +1,41 @@
+import { useEffect, useRef, useState } from "react";
 import tracks from "../data/tracks";
 
 export default function Player() {
+
+    const audioRef = useRef<HTMLAudioElement>(null);
+
+    const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (!isPlaying) {
+            audioRef.current?.pause();
+        } else {
+            audioRef.current?.play();
+        }
+    },[isPlaying, audioRef]);
+
+    function handleChangeTrack(file: string) {
+        if (audioRef.current) {
+            audioRef.current.src = `./tracks/${file}`;
+        }
+    }
+
     return (
         <div>
+            <div>
+                <button onClick={() => setIsPlaying((prev) => !prev)}>{isPlaying ? "Pause" : "Play"}</button>
+            </div>
+            <audio ref={audioRef} src="./tracks/09-IfYoureHappyChant.mp3" />
             <ul>
-                {tracks.map(track => (<li key={track.title}>{track.title}</li>))}
+                {tracks
+                    .map(track => (
+                        <li 
+                            style={{ cursor: "pointer" }}
+                            key={track.title}
+                            onClick={() => handleChangeTrack(track.file)}
+                        >{track.title}</li>
+                    ))}
             </ul>
         </div>
     );
